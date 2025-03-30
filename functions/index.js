ae0e0062d5233a634940d507318b1ed9108d5b45
WillianMacedo1919 <duchefffoods@gmail.com>
2025-03-29 21:38:52 -0300

    deuBoa

--- functions/index.js
+++ functions/index.js
@@ -1,26 +1,14 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
 
 const {onCall} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-exports.teste = onCall((data, context) => {
+exports.teste = onCall((request) => {
   logger.info("Hello logs!", {structuredData: true});
-  return {message: "Hello from Firebase!"};
+  return {text: "Hello from Function!", data:
+    {...request.data,
+      context: request.auth,
+    },
+  };
 });
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
